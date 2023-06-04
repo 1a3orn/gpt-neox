3d9556b2b35c71cd17877a26336f606a448a1db4
1a3orn <1a3orn@protonmail.com>
2023-06-03 20:16:35 -0400

    ASDAS

--- megatron/model/word_embeddings.py
+++ megatron/model/word_embeddings.py
@@ -55,7 +55,7 @@ class Embedding(torch.nn.Module):
         self.mup_rp_embedding_mult = neox_args.mup_rp_embedding_mult
 
         # Word embeddings (parallel).
-        DIVIDE = 24
+        DIVIDE = 96 * 2
         print("---------------------------------")
         print("Initializing word embeddings -- " + str(DIVIDE))
         print("---------------------------------")
